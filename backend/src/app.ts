6f3e075f996d407177a4d5b5a1d1b47e20650a27
Aggio98 <coder@aggioworld.com>
2025-12-14 12:26:52 +0000

    added express backend and first environment variable

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,22 @@
 //Express app setup
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = Number(process.env.PORT)
+
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Hello World");
+})
+
+app.listen(port, () =>{
+    console.log(`Server is connected to https://localhost:${port}`);
+})
+
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,2 @@
 //Server startup
+import "./app";
